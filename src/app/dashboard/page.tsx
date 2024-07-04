c4bafb13a9f41d1365f8aad0e7e080ce2725c797
Tommy Chiu <txmmychiu@gmail.com>
2024-07-04 16:47:02 -0400

    Setup NavBar component and begin implementing global style dark theme system

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React, { useState } from 'react';
+import { NavBar } from "../components/navbar"
+
+export default function Page() {
+    
+    return (
+        <div className="flex h-screen">
+            <div className="w-full">
+                <NavBar />
+            </div>
+      </div>
+    )
+
+}
